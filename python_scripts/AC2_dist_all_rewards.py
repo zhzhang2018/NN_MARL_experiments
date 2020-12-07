4a2e1317e434a18abbefd7746077f28b9e079a84
ZZmac <zhzhang@colby.edu>
2020-12-06 22:07:39 -0500

    Added files for Linux machine remote tests2

--- python_scripts/AC2_dist_all_rewards.py
+++ python_scripts/AC2_dist_all_rewards.py
@@ -16,12 +16,12 @@ import torch.nn.functional as F
 import torchvision.transforms as T
 from torch.autograd import Variable
 
-from ..utils.ReplayMemory import * 
-from ..utils.networks import *
-from ..utils.agents import *
-from ..utils.plotting import *
-from ..utils.train_test_methods import *
-from ..utils.params import *
+from .utils.ReplayMemory import * 
+from .utils.networks import *
+from .utils.agents import *
+from .utils.plotting import *
+from .utils.train_test_methods import *
+from .utils.params import *
 
 # Ask for input configurations
 taskname = input("Enter task name that would be used to name all outputs: ")
